b56ca7ce39622a3c6e1dd3ef2f25b4c94be56291
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-03-12 01:36:46 -0300

    Laraval 5.3 - Controllers Resources

--- app/Http/Controllers/Site/SiteController.php
+++ app/Http/Controllers/Site/SiteController.php
@@ -7,6 +7,15 @@ use App\Http\Controllers\Controller;
 
 class SiteController extends Controller
 {
+    
+    public function __construct() {
+        //$this->middleware('auth');
+        $this->middleware('auth')->only([
+           'contato',
+            'categoria'
+        ]);
+    }
+    
     public function index()
     {
         return 'Home Paga do site';

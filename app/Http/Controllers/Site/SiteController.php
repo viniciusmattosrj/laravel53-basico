d4dcae67e96047cf38bcbab50e2787229cdfb1d9
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-03-12 01:28:37 -0300

    Laraval 5.3 - Controllers

---
+++ app/Http/Controllers/Site/SiteController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SiteController extends Controller
+{
+    public function index()
+    {
+        return 'Home Paga do site';
+    }
+    
+    public function contato()
+    {
+        return 'Pg Contato';
+    }
+    
+    public function categoria($id)
+    {
+        return "Listagem dos Posts da categoria: {$id}";
+    }
+    
+    public function categoriaOp($id = 1)
+    {
+        return "Listagem dos Posts da categoria: {$id}";
+    }
+    
+    
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,9 @@
 <?php
 
-Route::get('/categoria/{idCat}/nome-fixo/{prm2}',function($idCat,$prm2){
-   return "Posts da categoria {$idCat} - {$prm2}"; 
-});
+Route::group(['namespace' => 'Site'], function() {
+    Route::get('/categoria/{id}', 'SiteController@categoria');
+    Route::get('/categoria2/{id?}', 'SiteController@categoriaOp');
 
-Route::get('/nome/nome2/nome6',function(){
-    return 'Rota Grande';
-})->name('rota.nomeada');
-
-Route::any('/any',function(){
-    return 'Route any';
-});
-
-Route::match(['get','post'], '/match',function(){
-    return 'Route match';
-});
-
-Route::post('/post',function(){
-   return 'Route Post'; 
-});
-
-Route::get('/contato',function(){
-   return 'Contato';
-});
-
-Route::get('/empresa',function(){
-   return view('empresa'); 
-});
-
-Route::get('/', function () {
-    //return view('welcome');
-    return redirect ()->route('rota.nomeada');
+    Route::get('/contato', 'SiteController@contato');
+    Route::get('/', 'SiteController@index');
 });

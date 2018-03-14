4b51a5452410b80e9dde9b54432233698d437786
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-03-14 00:50:33 -0300

    Laravel 5.3 - Blade @stacks

--- resources/views/site/home/index.blade.php
+++ resources/views/site/home/index.blade.php
@@ -54,6 +54,12 @@
     
     @include('site.includes.sidebar', compact('var1'))
     
+    
+    @push('scripts')
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    @endpush
+    
 
 
 @endsection()
--- resources/views/site/templates/template1.blade.php
+++ resources/views/site/templates/template1.blade.php
@@ -2,10 +2,12 @@
 <html>
     <head>
         <title>{{$title or 'Curso de Laravel 5.3 - Especializa TI'}}</title>
+        
     </head>
     <body>
         
         @yield('content')
         
+        @stack('scripts')
     </body>
 </html>

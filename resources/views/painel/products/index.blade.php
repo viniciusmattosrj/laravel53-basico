332a2f50b89d0976ca728e3c3c62062cca999a8c
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-04-05 00:07:56 -0300

    Laravel 5.3 - CSS

--- resources/views/painel/products/index.blade.php
+++ resources/views/painel/products/index.blade.php
@@ -2,17 +2,28 @@
 
 @section('content')
 
-<h1>Listagem dos produtos</h1>
+<h1 class="title-pg">Listagem dos produtos</h1>
 
-<table>
+<a href="" class="btn btn-primary btn-add"> <span class="glyphicon glyphicon-plus"></span>Cadastrar</a>
+
+<table class="table table-striped">
     <tr>
         <th>Nome</th>
         <th>Descrição</th>
+        <th width="100px">Ações</th>
     </tr>
     @foreach($products as $product)
         <tr>
             <td>{{$product->name}}</td>
             <td>{{$product->description}}</td>
+            <td>
+                <a href="" class="actions edit">
+                    <span class="glyphicon glyphicon-pencil"></span>
+                </a>
+                <a href="" class="actions delete">
+                    <span class="glyphicon glyphicon-trash"></span>
+                </a>
+            </td>
         </tr>   
     @endforeach
 </table>
